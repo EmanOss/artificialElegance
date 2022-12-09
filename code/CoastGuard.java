5dc50b3a2435e60bb325880daa5b8d042dc5b5ed
Iman <iman.osama2000@gmail.com>
2022-12-09 20:23:21 +0200

    math.random brackets

--- code/CoastGuard.java
+++ code/CoastGuard.java
@@ -64,15 +64,15 @@ public class CoastGuard extends GeneralSearch {
 
     public static String genGrid() {
         StringBuilder grid = new StringBuilder();
-        int n = ((int) Math.random() * 11) + 5; //5<=n<=15 - i - rows
-        int m = ((int) Math.random() * 11) + 5; //5<=m<=15 - j - columns
+        int n = (int) (Math.random() * 11) + 5; //5<=n<=15 - i - rows
+        int m = (int) (Math.random() * 11) + 5; //5<=m<=15 - j - columns
         grid.append(m + "," + n + ";");
 
         //randomize ships and stations
         // from 1 to n*m-2 to leave a cell for CG and a station
         int ships = ((int) (Math.random() * (m * n - 2))) + 1;
         //min between the random number and the remaining cells
-        int stations = Math.min(((int) Math.random() * (m * n - 2)) + 1, n * m - 1 - ships);
+        int stations = Math.min((int) (Math.random() * (m * n - 2)) + 1, n * m - 1 - ships);
 
         boolean[][] assigned = new boolean[n][m];
         for (int i = 0; i < stations + ships + 1; i++) {
@@ -84,13 +84,13 @@ public class CoastGuard extends GeneralSearch {
             }
             assigned[x][y] = true;
             if (i == 0)
-                grid.append((((int) Math.random() * 71) + 30) + ";" + x + "," + y + ";");
+                grid.append(((int) (Math.random() * 71) + 30) + ";" + x + "," + y + ";");
             else if (i < stations)
                 grid.append(x + "," + y + ",");
             else if (i == stations)
                 grid.append(x + "," + y + ";");
             else
-                grid.append(x + "," + y + "," + (((int) Math.random() * 100) + 1) + ",");
+                grid.append(x + "," + y + "," + ((int) (Math.random() * 100) + 1) + ",");
 
         }
 
